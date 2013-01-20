81a2ffdad67a3a3eadc3cbfb3c364218007034ed
Neil Green <neil@melessoftware.com>
2013-01-20 13:06:15 +0000

    install yum-plugin-fastestmirror early

--- fedora18.sh
+++ fedora18.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-yum install dkms gcc make kernel-devel wget
-yum update
+yum -y update
 
-#yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-18.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-18.noarch.rpm
-#rpm -ivh http://rpm.livna.org/livna-release.rpm
-yum install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-18.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-18.noarch.rpm
-yum install --nogpgcheck http://rpm.livna.org/livna-release.rpm
+yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-18.noarch.rpm
+yum -y install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-18.noarch.rpm
+yum -y install http://rpm.livna.org/livna-release.rpm
+yum -y install http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 
 wget -P /etc/yum.repos.d http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
-yum update
+
+#yum -y install https://linux.dropbox.com/packages/fedora/nautilus-dropbox-1.4.0-1.fedora.x86_64.rpm
+
+yum -y update
+
+yum -y install dkms gcc make kernel-devel wget vim openssh-server
+yum -y install VirtualBox-4.2
+yum -y install flash-plugin
+yum -y install akmod-nvidia
+
+yum -y update
+
