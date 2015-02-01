a73ff86f13cdaa6ec13fbaa731cfd62856b05d01
Neil Green <neil@melessoftware.com>
2015-02-01 16:35:59 +0000

    basic configuration of target system

---
+++ arch/configure.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+locale=en_GB.UTF-8
+sed -i'' "s/^#${locale}/${locale}/" '/etc/locale.gen'
+locale-gen
+echo LANG=${locale} > /etc/locale.conf
+export LANG=${locale}
+
+cat > /etc/vconsole.conf << EOF
+KEYMAP=uk
+FONT=Lat2-Terminus16
+EOF
+
+ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc --utc
+
+hostname=riesling
+echo $hostname > /etc/hostname
+sed "/^127\.0\.0\.1/ s/$/ ${hostname}/" /etc/hosts
+sed "/^::1/          s/$/ ${hostname}/" /etc/hosts
+
