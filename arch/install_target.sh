e1f2bea6918f9bfc2be486a04421a11c932ebdeb
Neil Green <neil@melessoftware.com>
2015-02-01 20:34:08 +0000

    cronie is required before creating cron jobs

--- arch/install_target.sh
+++ arch/install_target.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pacstrap -i /mnt base base-devel git zsh btrfs-progs vim networkmanager grub efibootmgr
+pacstrap -i /mnt base base-devel git zsh btrfs-progs vim networkmanager grub efibootmgr cronie
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
